7edf988d8c4ab944576a68e39ebfbf65efca1b02
Mateusz Suder <matisu999@gmail.com>
2021-12-29 15:33:17 +0100

    prepare controller endpoints

--- src/main/java/banking/application/controller/LoanController.java
+++ src/main/java/banking/application/controller/LoanController.java
@@ -1,9 +1,12 @@
 package banking.application.controller;
 
+import banking.application.annotation.Auth;
 import banking.application.util.SiteConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,5 +20,23 @@ public class LoanController extends Controller {
     public ResponseEntity GetLoanConfig() {
         SiteConfig siteConfig = new SiteConfig();
         return ResponseEntity.ok(siteConfig.getLoanConfig());
+    }
+
+    @Auth
+    @PostMapping("/take")
+    public ResponseEntity<?> TakeLoan() {
+        return ResponseEntity.ok(null);
+    }
+
+    @Auth
+    @PutMapping("/pay")
+    public ResponseEntity<?> PayLoan() {
+        return ResponseEntity.ok(null);
+    }
+
+    @Auth
+    @PutMapping("/autoPayment")
+    public ResponseEntity<?> SetAutoPayment() {
+        return ResponseEntity.ok(null);
     }
 }
