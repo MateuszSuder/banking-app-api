324decfcfc1d0cbe16761cf0e056b25f20cc79ab
Dawid Pers <dawid.g657@gmail.com>
2022-01-09 22:26:26 +0100

    add method that send money to saved recipient

--- src/main/java/banking/application/controller/RecipientController.java
+++ src/main/java/banking/application/controller/RecipientController.java
@@ -2,14 +2,18 @@ package banking.application.controller;
 
 import banking.application.annotation.Auth;
 import banking.application.exception.ThrowableErrorResponse;
+import banking.application.model.Rate;
 import banking.application.model.Recipient;
+import banking.application.model.input.TransferInput;
 import banking.application.util.AccountType;
 import banking.application.util.ErrorResponse;
+import banking.application.util.TransactionType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Controller handling saved recipients
@@ -73,6 +77,41 @@ public class RecipientController extends Controller {
         } catch (ThrowableErrorResponse e) {
             return ResponseEntity.status(e.code).body(e.getErrorResponse());
         }
+    }
+
+    /**
+     * Endpoint handling sending money to saved recipient
+     * @param accountType type of account
+     * @param transferInput transferInput
+     * @return balance after money transfer
+     */
+    @Auth
+    @PostMapping("/{accountType}/send")
+    ResponseEntity<?> SendToRecipient(@PathVariable AccountType accountType, @Valid @RequestBody TransferInput transferInput) {
+        String iban = this.currentUser.getCurrentUser().getUserAccounts().getIban(accountType);
+        List<Recipient> recipients = this.recipientService.getAllRecipients(iban);
+        Recipient recipient = recipients.stream()
+                .filter(r -> r.getAccountNumber().equals(transferInput.getTo().getAccountNumber()))
+                .findAny()
+                .orElse(null);
+        if(recipient == null) {
+            return ResponseEntity.status(404).body(new ErrorResponse(
+                    "Not found",
+                    "Recipient not bound to an account",
+                    404));
+        }
+
+        try {
+            return ResponseEntity.status(200).body(this.accountService.transferMoney(
+                    iban,
+                    transferInput.getTo(),
+                    transferInput.getValue(),
+                    transferInput.getTitle(),
+                    TransactionType.STANDING_ORDER));
+        } catch (ThrowableErrorResponse e) {
+            return ResponseEntity.status(e.code).body(e.getErrorResponse());
+        }
+
 
     }
 }
