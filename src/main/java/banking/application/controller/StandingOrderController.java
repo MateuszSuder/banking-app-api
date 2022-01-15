c82be32e3ecf61cf96eb5a656e0d49ecad358ec2
Mateusz Suder <matisu999@gmail.com>
2022-01-15 20:04:26 +0100

    add comments

--- src/main/java/banking/application/controller/StandingOrderController.java
+++ src/main/java/banking/application/controller/StandingOrderController.java
@@ -14,18 +14,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/standing")
 public class StandingOrderController extends Controller {
 
+	/**
+	 * Add standing order to user's account which type is given in path variable
+	 * Standing order input is given in request's body
+	 * @param standingOrderInput standing order information
+	 * @param accountType account type from which to delete
+	 * @return list of account's standing orders
+	 */
 	@Auth
-	@PostMapping("/")
-	ResponseEntity<?> AddStandingOrder(@RequestBody StandingOrderInput standingOrderInput) {
+	@PostMapping("/{accountType}")
+	ResponseEntity<?> AddStandingOrder(@PathVariable AccountType accountType, @RequestBody StandingOrderInput standingOrderInput) {
 		return ResponseEntity.ok(null);
 	}
 
+	/**
+	 * Delete standing order of user's account which type is given in path variable
+	 * @param accountType account type of which to delete from
+	 * @param id id of standing order to delete
+	 * @return list of account's standing orders
+	 */
 	@Auth
 	@DeleteMapping("/{accountType}/{id}")
 	ResponseEntity<?> DeleteStandingOrder(@PathVariable AccountType accountType, @PathVariable String id) {
 		return ResponseEntity.ok(null);
 	}
 
+	/**
+	 * Modify existing standing order
+	 * @param accountType type account from which to modify
+	 * @param id id of standing order to modify
+	 * @param standingOrderInput modify input
+	 * @return list of account's standing orders
+	 */
 	@Auth
 	@PutMapping("/{accountType}/{id}")
 	ResponseEntity<?> ModifyStandingOrder(@PathVariable AccountType accountType, @PathVariable String id, @RequestBody StandingOrderInput standingOrderInput) {
---
+++ src/main/java/banking/application/service/StandingOrderService.java
@@ -0,0 +1,7 @@
+package banking.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StandingOrderService extends EntryService{
+}
